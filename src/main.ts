77c385e6f2ffceab4c1cd403850c19b1746db0ec
lesguillemets <kgkacko+github@gmail.com>
2024-11-22 21:18:01 +0900

    reorder

--- src/main.ts
+++ src/main.ts
@@ -2,14 +2,8 @@ function init() {
 	// 基本的に最後に押したキーだけ考えるので，それだけ覚えとくようにする
 	// …と思ったけど，video を動かすためのカーソルは無視したいな
 	// let lastPressedKey: string = "";
+	// remember currently-pressed keys
 	const pressedKeys: Set<string> = new Set([]);
-	const canv: HTMLCanvasElement = document.getElementById(
-		"record-canv",
-	)! as HTMLCanvasElement;
-	const canvHeight: number = canv.height;
-	const canvWidth: number = canv.width;
-	const ctx: CanvasRenderingContext2D = canv.getContext("2d")!;
-	canvInit(canv);
 	window.addEventListener("keydown", (e) => {
 		pressedKeys.add(e.key);
 		// console.log(lastPressedKey);
@@ -18,10 +12,19 @@ function init() {
 		pressedKeys.delete(e.key);
 		// console.log(lastPressedKey);
 	});
+	// prepare canvases
+	const canv: HTMLCanvasElement = document.getElementById(
+		"record-canv",
+	)! as HTMLCanvasElement;
+	const canvHeight: number = canv.height;
+	const canvWidth: number = canv.width;
+	const ctx: CanvasRenderingContext2D = canv.getContext("2d")!;
+	canvInit(canv);
 	console.log("initialised");
 }
 
 function canvInit(canv: HTMLCanvasElement) {
+	// clears the canvas and draws horizontal line
 	const ctx: CanvasRenderingContext2D = canv.getContext("2d")!;
 	const w: number = canv.width;
 	const h: number = canv.height;
