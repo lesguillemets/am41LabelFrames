32e73c6a637601e8677a7172dcbd2ed0b7055bd0
lesguillemets <kgkacko+github@gmail.com>
2024-11-22 21:04:22 +0900

    save keypresses

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,23 @@
 function init() {
-	alert("HI");
+	// 基本的に最後に押したキーだけ考えるので，それだけ覚えとくようにする
+	// …と思ったけど，video を動かすためのカーソルは無視したいな
+	// let lastPressedKey: string = "";
+	const pressedKeys: Set<string> = new Set([]);
+	const canv: HTMLCanvasElement = document.getElementById('record-canv')! as HTMLCanvasElement;
+	const ctx: CanvasRenderingContext2D = canv.getContext('2d')!;
+	window.addEventListener(
+		'keydown', (e) => {
+			pressedKeys.add(e.key);
+			// console.log(lastPressedKey);
+		}
+	);
+	window.addEventListener(
+		'keyup', (e) => {
+			pressedKeys.delete(e.key);
+			// console.log(lastPressedKey);
+		}
+	);
+	console.log('initialised');
 }
 
 window.addEventListener("load", init);
