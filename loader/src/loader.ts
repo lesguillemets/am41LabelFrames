b7cfe2a24b3e6ab61e91eda2d281862cfe9c08d1
lesguillemets <kgkacko+github@gmail.com>
2024-12-04 15:37:12 +0900

    loading files

--- loader/src/loader.ts
+++ loader/src/loader.ts
@@ -1,3 +1,5 @@
+const LABEL_COLOURS = ["#ffffff", "#7022ba", "#f0bd30", "#de4a18"];
+
 function init() {
 	prepareLoadingVideo();
 }
@@ -10,6 +12,7 @@ function prepareLoadingVideo() {
 		"the-video",
 	)! as HTMLSourceElement;
 	let loaded = false;
+	// redraw background after loading video
 	vinput.addEventListener("change", (e) => {
 		console.log(`loading video: ${e}`);
 		if (!loaded && vinput.files !== null) {
@@ -18,7 +21,7 @@ function prepareLoadingVideo() {
 			document
 				.getElementById("the-video")!
 				.addEventListener("loadedmetadata", () => {
-					initLabeller();
+					drawPlayerBackGround();
 				});
 			loaded = true;
 		} else if (loaded) {
@@ -27,8 +30,52 @@ function prepareLoadingVideo() {
 			console.log("not yet loaded and input is null");
 		}
 	});
+
+	// redraw background after loading label files
+	const lfInput: HTMLInputElement = document.getElementById(
+		"label-files",
+	)! as HTMLInputElement;
+	lfInput.addEventListener("change", (e) => {
+		drawPlayerBackGround();
+	});
+	// draw current time wheneevr the video is played
 }
 
-function initLabeller(){}
+function drawPlayerBackGround() {
+	// getting relevant elements
+	// video
+	const video: HTMLMediaElement = document.getElementById(
+		"the-video",
+	)! as HTMLMediaElement;
+
+	// canvas to draw on
+	const canv: HTMLCanvasElement = document.getElementById(
+		"label-canv",
+	)! as HTMLCanvasElement;
+	const canvHeight: number = canv.height;
+	const canvWidth: number = canv.width;
+	const ctx: CanvasRenderingContext2D = canv.getContext("2d")!;
+
+	// input
+	const lfInput: HTMLInputElement = document.getElementById(
+		"label-files",
+	)! as HTMLInputElement;
+	const labelFiles: FileList = lfInput.files!;
+
+	const nFiles: number = labelFiles.length;
+	const barHeight: number = canvHeight / nFiles;
+	for (let i = 0; i < nFiles; i++) {
+		drawLabels(canv, labelFiles[i], barHeight, i);
+	}
+}
+
+function drawLabels(
+	canv: HTMLCanvasElement,
+	labelFile: File,
+	barHeight: number,
+	i: number,
+) {
+	console.log(labelFile);
+}
 
 window.addEventListener("load", init);
